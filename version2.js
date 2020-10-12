9487e20a52869951a09887f86cc5030c497c6b76
hi <kangja1996@gmail.com>
2020-10-12 11:11:56 -0400

    progress

--- version2.js
+++ version2.js
@@ -1,7 +1,31 @@
 function diffArray(arr1, arr2) {
   const union = [];
-  
+  for (let i = 0; i < arr1.length; i++) {
+    if (!union.includes(arr1[i])) {
+      union.push(arr1[i]);
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (!union.includes(arr2[i])) {
+      union.push(arr2[i]);
+    }
+  }
+  console.log("The value of union is " + union)
+  // The value of union is 1,2,3,5,4
+
+  const symmDiff = [];
+  for (let i = 0; i < union.length; i++) {
+    const currentEl = union[i]; 
+
+    if (arr1.includes(currentEl) && !arr2.includes(currentEl)) {
+      symmDiff.push(currentEl);
+    } else if (arr2.includes(currentEl) && !arr1.includes(currentEl)) {
+      symmDiff.push(currentEl);
+    }
+  }
+
+  return symmDiff;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 // [4]
